4c9ee12733bb3d68b0cc36455a1996352bd14cfb
Alexander Bashenin <timebender00@gmail.com>
2025-03-03 19:03:38 +0400

    primeng

---
+++ src/app/layout/content/content.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+    selector: 'app-content',
+    imports: [CommonModule],
+    templateUrl: './content.component.html',
+    styleUrls: ['./content.component.scss'],
+})
+export class ContentComponent {}
